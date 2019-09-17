5f615547de6dfa1930cd9e4d3551c618d3da9a8c
Adalberto Ap <betoap@msn.com>
2019-09-17 14:00:31 -0300

    remover text Resolve

--- annotations/resolve-trivial.js
+++ annotations/resolve-trivial.js
@@ -4,7 +4,7 @@ const resolve_trivial_1 = require("./../classes/resolve-trivial");
 function resolveTrivial(target, key, descriptor) {
     var originalMethod = descriptor.value;
     const name = target.constructor.name;
-    const objName = `${name.charAt(0).toLowerCase()}${name.substr(1)}`.replace('Graphql', '');
+    const objName = `${name.charAt(0).toLowerCase()}${name.substr(1)}`.replace('Graphql', '').replace('Resolve', '');
     resolve_trivial_1.ResolveTrivial.getInstance().append(objName, key, originalMethod);
     return descriptor;
 }
--- src/annotations/resolve-trivial.ts
+++ src/annotations/resolve-trivial.ts
@@ -3,7 +3,7 @@ import { ResolveTrivial } from './../classes/resolve-trivial';
 export function resolveTrivial(target: any, key: string, descriptor: any): any {
     var originalMethod = descriptor.value;
     const name = target.constructor.name;
-    const objName = `${name.charAt(0).toLowerCase()}${name.substr(1)}`.replace( 'Graphql', '' );
+    const objName = `${name.charAt(0).toLowerCase()}${name.substr(1)}`.replace( 'Graphql', '' ).replace( 'Resolve', '' );
     ResolveTrivial.getInstance().append(objName, key, originalMethod);
     return descriptor;
 }
